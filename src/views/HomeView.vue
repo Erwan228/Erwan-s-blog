a9a36a5226105dc9e65063cfb18872d1b4b13756
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-29 10:32:40 +0200

    gf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,30 @@
   <div class="home">
     <h1>Home</h1>
     <h2>Blog posts</h2>
+      <div class="blogs"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { onMounted, ref } from 'vue';
+
 
 export default {
   name: 'HomeView',
   components: {
     
+  },
+  setup() {
+    const blogs = ref([])
+    onMounted(() => {
+      fetch('http://localhost:3000/blogs')
+      .then(res => res.json())
+      .then(data => this.blogs = data)
+      .catch(err => console.log(err.message))
+    })
+    return {
+      blogs,
+    }
   }
 }
 </script>
